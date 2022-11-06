9914ee06b4adf6da5ea7086c453e05dce066f973
Adebayo Yusuf <offisial007@outlook.com>
2022-11-06 23:00:10 +0100

    update

---
+++ flaskblog/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd53d5f616c4d1c4dfd254b1b4dd247cf'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from flaskblog import routes
